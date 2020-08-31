b59603d686d75049e2445813caad971df7eca793
Ashik <ashikroyce@gmail.com>
2020-08-31 19:13:26 +0530

    github secrets

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        <p className="footer-text">{"Made with 🤍 by Jainam Desai"}</p>
+        <p className="footer-text">{"Made with 🤍 by Ashik!!!"}</p>
       </div>
     </Fade>
   );
