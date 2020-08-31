bce9743c3ef0ee4569126ce927be0e2c6b5b213f
Ashik <ashikroyce@gmail.com>
2020-08-31 18:01:53 +0530

    Updates

---
+++ precache-manifest.c45ffe56705cda9645184aa03e1f34ff.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26537c03e96c17a7490765b86c0c6ee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c3f6e5423270dadd5b4",
+    "url": "/static/css/main.3b55edbd.chunk.css"
+  },
+  {
+    "revision": "eee7553ddbbbc93fc6b6",
+    "url": "/static/js/2.a609288e.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.a609288e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ae7c6c3894389e5a31",
+    "url": "/static/js/3.9af890cb.chunk.js"
+  },
+  {
+    "revision": "d58ab803403d2a1035fc",
+    "url": "/static/js/4.6a9a35e7.chunk.js"
+  },
+  {
+    "revision": "6c3f6e5423270dadd5b4",
+    "url": "/static/js/main.a96218fa.chunk.js"
+  },
+  {
+    "revision": "9d535606ba62d3084e16",
+    "url": "/static/js/runtime-main.7185ec5b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "19f732c21334be1d4334225037837156",
+    "url": "/static/media/developerActivity.19f732c2.svg"
+  },
+  {
+    "revision": "1ce63cf269592c35130c567d8ac2bce5",
+    "url": "/static/media/programmer.1ce63cf2.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
